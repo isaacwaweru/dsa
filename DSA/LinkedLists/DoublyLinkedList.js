24edd18d0e74897fa63c312478466ca92d55f77e
Isaac Waweru <waweruisaac25@gmail.com>
2026-05-16 01:34:49 +0300

    ll

---
+++ DSA/LinkedLists/DoublyLinkedList.js
@@ -0,0 +1,121 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    const poppedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    const oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+      oldHead.prev = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let count, current;
+
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+
+    return current;
+  }
+}
+
+const list = new DoublyLinkedList();
+
+list.push(99);
+list.push(100);
+list.push(105);
+list.push(110);
+list.push(115);
+list.push(120);
+
+// console.log(list);
+// console.log(list.pop());
+// console.log(list.shift());
+// console.log(list.unshift(90));
+// console.log(list.get(5));
+// console.log(list);
